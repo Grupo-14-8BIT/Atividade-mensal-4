07d8b818e4a4f0fcf0f520d67beae2ec1f6f6a66
Kript0n007 <gabrielkuhnen38@gmail.com>
2022-12-11 14:32:48 -0300

    update

---
+++ teste2/teste.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     var $InputLogin = $("#login");
+// $InputLogin.click(function(){
+// if ($InputLogin.hasClass("dourado"))
+//     $InputLogin.addClass("branco").removeClass("dourado");
+// else
+//     $InputLogin.addClass("dourado").removeClass("branco");
+// });
+// })
