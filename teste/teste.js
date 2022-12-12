a72f6f2745550d2ba9314bb6a7301455d574b44a
Kript0n007 <gabrielkuhnen38@gmail.com>
2022-12-12 12:16:16 -0300

    update

--- teste/teste.js
+++ teste/teste.js
@@ -21,7 +21,7 @@ function carregar_jogos(categoria, plataforma) {
             console.log(response[0]);
             for (var i = 0; i < lista_jogos; i++) {
 
-                jogos.innerHTML += '<div class="jogo"><div class="imagem-jogo"><a href="' + response[i].game_url + '" target="blank"><img src="' + response[i].thumbnail + '" alt="anal"> </a><input type="checkbox" id="cm_star-empty" >  <label for="cm_star-empty"><i class="fa"></i></label></div><p>' + response[i].title + '</p></div>';
+                jogos.innerHTML += '<div class="jogo"><div class="imagem-jogo"><a href="' + response[i].game_url + '" target="blank"><img src="' + response[i].thumbnail + '" alt="anal"> </a><input type="checkbox" id="cm_star-empty'+[i]+'"> <label for=cm_star-empty'+[i]+'><i class="fa"></i></label></div><p>' + response[i].title + '</p></div>';
 
             }
         }).catch(err => console.error(err));
