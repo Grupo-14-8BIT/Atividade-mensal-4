7a34f62fde8b0b983e45da0961aac6413c5c0b19
Hasanessaad <hasancayedessaad@gmail.com>
2022-12-15 18:02:57 -0300

    done

--- teste/teste.js
+++ teste/teste.js
@@ -1,7 +1,6 @@
 const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.getElementById("search_id");
-const lol = "";
 let games;
 let users = [];
 
@@ -9,11 +8,7 @@ searchInput.addEventListener("input", habib => {
 
     const value = habib.target.value.toLowerCase();
     jogos.innerHTML = '';
-    // function remove(){
-    //     lol = document.getElementById("search_id").innerText;
-    //     value="";
-    //     userCardContainer.removeChild(card);
-    // }
+
 if (users != '') {
         users.forEach(user => {
             const isVisible = user.title.toLowerCase().includes(value);
@@ -54,7 +49,13 @@ if (users != '') {
 
 });
 
-
+function remove(){
+    searchInput.value = "";
+    carregar_jogos("home", "all");
+    users.forEach(user =>{
+        user.element.classList.toggle("hide");
+    })
+}
 
 // variaveis para exibir os jogos
 let plataforma;
